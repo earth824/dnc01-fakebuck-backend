9dd2db6efd311e24b50ed556a065978233647cf1
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-25 15:59:54 +0700

    feat: Add security module and hash service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { SecurityModule } from './shared/security/security.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, AuthModule, UserModule],
+  imports: [ConfigModule, DatabaseModule, AuthModule, UserModule, SecurityModule],
   providers: [{ provide: APP_GUARD, useClass: AuthGuard }]
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/database/generated/prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
+import { BcryptService } from 'src/shared/security/services/bcrypt.service';
 import { CreateUserDto } from 'src/user/dtos/create-user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly bcryptService: BcryptService
+  ) {}
 
-  create(createUserDto: CreateUserDto): Promise<User> {
-    // hash password
-    const hashedPassword = 'xxx';
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await this.bcryptService.hash(
+      createUserDto.password
+    );
+
     return this.prisma.user.create({
       data: { ...createUserDto, password: hashedPassword }
     });

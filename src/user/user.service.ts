c07be5ed1f06f2d4dd9928556a3ef24bb87402e6
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-25 16:25:48 +0700

    feat: Hash a password and catch duplicate email error

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from 'src/database/generated/prisma/client';
+import { PrismaClientKnownRequestError } from 'src/database/generated/prisma/internal/prismaNamespace';
 import { PrismaService } from 'src/database/prisma.service';
 import { BcryptService } from 'src/shared/security/services/bcrypt.service';
 import { CreateUserDto } from 'src/user/dtos/create-user.dto';
@@ -16,8 +17,23 @@ export class UserService {
       createUserDto.password
     );
 
-    return this.prisma.user.create({
-      data: { ...createUserDto, password: hashedPassword }
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: { ...createUserDto, password: hashedPassword }
+      });
+      return user;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException({
+          message: `Email: ${createUserDto.email} is already in use`,
+          code: 'EMAIL_ALREADY_EXISTS'
+        });
+      }
+
+      throw error;
+    }
   }
 }
